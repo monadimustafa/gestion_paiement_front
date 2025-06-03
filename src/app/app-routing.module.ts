ce102cebc6aff357fde1b4818f620390c9eb0bd2
monadimustafa <monadi.mustafa@gmail.com>
2025-06-03 23:00:04 +0000

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './core/components/home/home.component';
 import { ProfileComponent } from './core/components/profile/profile.component';
 import { LoginComponent } from './core/components/login/login.component';
